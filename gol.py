70b11c9bfd6ff98cfa5a1256679f0ff4057aca18
Fred Bunt <fredrick.bunt@gmail.com>
2021-01-19 08:23:29 -0700

    update

--- gol.py
+++ gol.py
@@ -11,90 +11,62 @@ def random_init(shape, p=0.2):
     return (np.random.random(shape) < p).astype(np.uint8)
 
 
-def block_init(shape):
-    """Initialize state with the block formation
+# https://www.conwaylife.com/wiki/Glider
+PATTERN_GLIDER = """
+010
+001
+111
+"""
+# https://www.conwaylife.com/wiki/Pentadecathlon
+PATTERN_PENTADECATHLON = """
+11111111
+10111101
+11111111
+"""
+# https://www.conwaylife.com/wiki/Acorn
+PATTERN_ACORN = """
+0100000
+0001000
+1100111
+"""
+# https://www.conwaylife.com/wiki/24827M
+PATTERN_24827M = """
+00000010
+00010100
+10000100
+01100000
+00101000
+00010000
+00000111
+"""
+PATTERN_MAP = {
+    "glider": PATTERN_GLIDER,
+    "acorn": PATTERN_ACORN,
+    "pent": PATTERN_PENTADECATHLON,
+    "meth": PATTERN_24827M,
+}
 
-    https://www.conwaylife.com/wiki/Block
-    """
+
+def pattern_to_array(pat):
+    pat = pat.strip()
+    arr = []
+    for line in pat.split():
+        row = []
+        for c in line:
+            row.append(int(c))
+        arr.append(row)
+    arr = np.array(arr, dtype=np.uint8)
+    return arr
+
+
+def init_from_pattern(shape, pat):
     state = np.zeros(shape, dtype=np.uint8)
-    i = shape[0] // 2
-    j = shape[1] // 2
-    state[i : i + 2, j : j + 2] = 1
-    return state
-
-
-def init_beehive(shape):
-    """Initialize state with the beehive formation
-
-    https://www.conwaylife.com/wiki/Beehive
-    """
-    state = np.zeros(shape, dtype=np.uint8)
-    i = shape[0] // 2
-    j = shape[1] // 2
-    state[i, j : j + 2] = 1
-    state[i + 2, j : j + 2] = 1
-    state[i + 1, j - 1] = 1
-    state[i + 1, j + 2] = 1
-    return state
-
-
-def init_glider(shape):
-    """Initialize state with the glider formation
-
-    https://www.conwaylife.com/wiki/Glider
-    """
-    state = np.zeros(shape, dtype=np.uint8)
-    i = shape[0] // 2
-    j = shape[1] // 2
-    state[i - 1, j] = 1
-    state[i, j + 1] = 1
-    state[i + 1, j - 1 : j + 2] = 1
-    return state
-
-
-def init_pentadecathlon(shape):
-    """Initialize state with the pentadecathlon formation
-
-    https://www.conwaylife.com/wiki/Pentadecathlon
-    """
-    state = np.zeros(shape, dtype=np.uint8)
-    i = shape[0] // 2
-    j = shape[1] // 2
-    state[i - 1 : i + 2, j - 3 : j + 5] = 1
-    state[i, j - 2] = 0
-    state[i, j + 3] = 0
-    return state
-
-
-def init_acorn(shape):
-    """Initialize state with the acorn formation
-
-    https://www.conwaylife.com/wiki/Acorn
-    """
-    state = np.zeros(shape, dtype=np.uint8)
-    i = shape[0] // 2
-    j = shape[1] // 2
-    state[i, j] = 1
-    state[i - 1, j - 2] = 1
-    state[i + 1, j - 3 : j + 4] = 1
-    state[i + 1, j - 1 : j + 1] = 0
-    return state
-
-
-def init_24827M(shape):
-    state = np.zeros(shape, dtype=np.uint8)
-    i = shape[0] // 2
-    j = shape[1] // 2
-    state[i - 3, j + 2] = 1
-    state[i - 2, j - 1] = 1
-    state[i - 2, j + 1] = 1
-    state[i - 1, j - 4] = 1
-    state[i - 1, j + 1] = 1
-    state[i, j - 3 : j - 1] = 1
-    state[i + 1, j - 2] = 1
-    state[i + 1, j] = 1
-    state[i + 2, j - 1] = 1
-    state[i + 3, j + 1 : j + 4] = 1
+    pat = pattern_to_array(pat)
+    i, j = [x // 2 for x in shape]
+    n, m = pat.shape
+    istart = i - (n // 2)
+    jstart = j - (m // 2)
+    state[istart : istart + n, jstart : jstart + m] = pat
     return state
 
 
@@ -160,10 +132,6 @@ class SimulationRunner:
             pass
 
 
-class MaxIterException(Exception):
-    pass
-
-
 class SimulationAnimation:
     def __init__(self, sim, interval, max_iter):
         self.sim = sim
@@ -212,22 +180,30 @@ class SimulationAnimation:
         plt.show()
 
 
-def density_plot(sim):
-    sns.set_style("whitegrid")
-    plt.figure()
-    plt.plot(sim.density)
-    plt.title("Density Over Time")
-    plt.xlabel("Generation")
-    plt.ylabel("Density")
-
+def density_vs_plot(sim):
     fig = plt.figure()
     ax = fig.add_subplot(111, aspect="equal")
     ax.plot(sim.density_tp1, sim.density_t)
     plt.title("Density(t+1) vs Density(t)")
     plt.xlabel("Density(t+1)")
     plt.ylabel("Density(t)")
-
     plt.show()
+
+
+def density_over_time_plot(sim):
+    sns.set_style("whitegrid")
+    plt.figure()
+    plt.plot(sim.density)
+    plt.title("Density Over Time")
+    plt.xlabel("Generation")
+    plt.ylabel("Density")
+    plt.show()
+
+
+def validate_pattern(pat):
+    if pat in PATTERN_MAP:
+        return pat
+    raise KeyError("Invalid pattern name. Use -h/--help to view valid options")
 
 
 def get_cli_parser():
@@ -266,28 +242,44 @@ def get_cli_parser():
         default=np.inf,
         help="Max number of iterations. Default: inf",
     )
+    opts = f"{[k for k in PATTERN_MAP]}".strip("[]")
+    p.add_argument(
+        "--pat",
+        type=validate_pattern,
+        default=None,
+        help=f"Starting pattern to use instead of random. Options are: {opts}",
+    )
     return p
 
 
-def main(args):
-    n = args.grid_size
+def run(
+    grid_size=400,
+    prob=0.5,
+    pat=None,
+    max_iter=np.inf,
+    interval=1,
+    headless=True,
+):
+    n = grid_size
     shape = (n, n)
-    p = args.prob
-    state = random_init(shape, p=p)
-    # state = init_block(shape)
-    # state = init_beehive(shape)
-    # state = init_glider(shape)
-    # state = init_pentadecathlon(shape)
-    # state = init_acorn(shape)
-    # state = init_24827M(shape)
+    if pat is not None:
+        state = init_from_pattern(shape, PATTERN_MAP[pat])
+    else:
+        state = random_init(shape, p=prob)
     sim = GOLSimulation(state)
     runner = None
-    if args.headless:
-        runner = SimulationRunner(sim, args.max_iter)
+    if headless:
+        runner = SimulationRunner(sim, max_iter)
     else:
-        runner = SimulationAnimation(sim, args.interval, args.max_iter)
+        runner = SimulationAnimation(sim, interval, max_iter)
     runner.run()
-    # density_plot(sim)
+    return sim
+
+
+def main(args):
+    sim = run(**vars(args))
+    density_over_time_plot(sim)
+    density_vs_plot(sim)
 
 
 if __name__ == "__main__":
